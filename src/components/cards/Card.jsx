a8dc41161b59aeed6b44695376719aafbd0235c8
eyupGuc <meydanguresi@gmail.com>
2022-10-11 09:57:00 +0300

    move

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -2,7 +2,7 @@ import "./Card.css";
 const Card = (props) => {
   const { key, title, desc, img } = props;
   return (
-    <div className="card">
+    <div className=" card">
       <h2>{title}</h2>
       <img src={img} alt={title} />
       <p>{desc}</p>
